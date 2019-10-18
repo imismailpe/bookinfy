d7e3637c308158c04a323ce1960af0c84f6e0d37
Ismail <mailismailpe@gmail.com>
2019-10-18 07:33:23 +0530

    Updates

---
+++ precache-manifest.16da6a93a9066f99a2ccad1650a64320.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a455de488cb4cea8b4b78d76e9c25bf6",
+    "url": "/bookinfy/index.html"
+  },
+  {
+    "revision": "49ea8dc693e66cddd80d",
+    "url": "/bookinfy/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e77073279ec3f8b8ca44",
+    "url": "/bookinfy/static/js/2.0e199036.chunk.js"
+  },
+  {
+    "revision": "49ea8dc693e66cddd80d",
+    "url": "/bookinfy/static/js/main.08668959.chunk.js"
+  },
+  {
+    "revision": "d14277b5d8f00be4f225",
+    "url": "/bookinfy/static/js/runtime-main.f4d1e264.js"
+  }
+]);
